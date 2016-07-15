12a8218cd734edc37d67ee2c56e5bce04545e78f
praneethkumarpidugu <pidugu2007@gmail.com>
2016-07-14 23:02:41 -0400

    http basic design

--- fictional-web-design-agency/script.js
+++ fictional-web-design-agency/script.js
@@ -8,12 +8,30 @@ var app = angular.module("computer", ['ngRoute'])
         when('/main', {
         templateUrl: 'main.html',
         controller: 'MainCtrl'
+    }). when('/about', {
+        templateUrl: 'about.html',
+        controller: 'MainCtrl'
+    }). when('/services', {
+        templateUrl: 'services.html',
+        controller: 'ServicesCtrl'
+    }). when('/contact', {
+        templateUrl: 'contact.html',
+        controller: 'ContactCtrl'
     }).
         otherwise({redirectTo:'/main'})
 }])
 
 .controller('MainCtrl', ['$scope',function ($scope) {
-    $scope.person = 'someperson';
-    console.log($scope);
+
+    
+}])
+
+.controller('ServicesCtrl', ['$scope', '$http', function ($scope, $http) {
+    $http.get('services.json').then(function (response) {
+        $scope.services = response.data;
+    })
+}])
+
+.controller('ContactCtrl', ['$scope', function ($scope) {
     
 }]);
